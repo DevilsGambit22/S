d54cb233adb0c9f474fdb22a7892c29e6e120acb
DevilsGambit22 <isaiasflash@hotmail.com>
2026-07-31 14:36:51 -0500

    Delete data directory

--- data/playlist.js
+++
@@ -1,6 +0,0 @@
-window.ANHS_PLAYLIST = [
-  {title:'Protocol Opening',artist:'ANHS Radio',file:'assets/music/protocol-opening.mp3',cover:'assets/images/default-cover.svg',genre:'System'},
-  {title:'Midnight Academy',artist:'ANHS Radio',file:'assets/music/midnight-academy.mp3',cover:'assets/images/default-cover.svg',genre:'Dark'},
-  {title:'Tokyo Signal',artist:'ANHS Radio',file:'assets/music/tokyo-signal.mp3',cover:'assets/images/default-cover.svg',genre:'J-Pop'},
-  {title:'Merit Engine',artist:'ANHS Radio',file:'assets/music/merit-engine.mp3',cover:'assets/images/default-cover.svg',genre:'Phonk'}
-];
